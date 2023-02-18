fb6835f385abd502b61565631ee8e75b7e502702
Micha Hanselmann <micha.hanselmann@gmail.com>
2023-02-18 18:30:21 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,13 @@
+def add(x, y):
+    return x + y
+
+
+def main():
+    x = 1
+    y = 2
+    result = add(x, y)
+    print(f"This is the sum: {x}, {y}, {result}")
+
+
+if __name__ == "__main__":
+    main()
